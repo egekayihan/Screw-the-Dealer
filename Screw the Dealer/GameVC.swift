3806b0153340b8beef4217af9ae2c576bee3393f
ek223gi <ek223gi@student.lnu.se>
2023-08-11 19:53:49 +0300

    Previous Cards feature does not work. Design needs update. End VC fixed

--- Screw the Dealer/GameVC.swift
+++ Screw the Dealer/GameVC.swift
@@ -11,9 +11,11 @@ class GameVC: UIViewController {
 
     @IBOutlet var cardImageView: UIImageView!
     
-    var deck: [UIImage] = []
-    
-    var prevCards: [UIImage] = []
+    var deck: [String] = []
+    var currentCard: UIImage = UIImage(named: "card-cover")!
+    var currentCardValue: String = "card-cover"
+    var prevCards: [String] = []
+    //var defaults = NSUserDefaults.standardUserDefaults
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,16 +26,35 @@ class GameVC: UIViewController {
         
     }
 
+    @IBAction func PrevCardsAction(segue: UIStoryboardSegue, _ sender: UIButton) {
+        let destinationVC = segue.destination as! PreviousCardsVC
+            destinationVC.passedPrevCards = prevCards
+    }
+    
     @IBAction func DrawCardAction(_ sender: Any) {
         
         if (deck.isEmpty) {
-        self.navigationController?.pushViewController(EndVC(), animated: true)
+            performSegue(withIdentifier: "toEndVC", sender: UIButton.self)
         } else {
-            cardImageView.image = deck.randomElement()
+            //cardImageView.image = deck.randomElement()
+            currentCardValue = deck.randomElement()!
+            currentCard = UIImage(named: currentCardValue)!
+            cardImageView.image = currentCard
             
-            deck.remove(at: deck.firstIndex(of: cardImageView.image!)!)
-            prevCards.append(cardImageView.image!)
+            deck.remove(at: deck.firstIndex(of: currentCardValue)!)
+            prevCards.append(currentCardValue)
+            
+            print(prevCards)
+            
+            //defaults.setObject(prevCards, forKey: "prevCards")
         }
     }
+    
+    
+    
+    /*@IBAction func performUnwindSegueOperation(_ sender: UIStoryboardSegue) {
+    guard let landingVC = sender.source as? PreviousCardsVC else { return }
+        landingVC.prevCards = prevCards
+    } */
     
 }
--- Screw the Dealer/PreviousCardsVC.swift
+++ Screw the Dealer/PreviousCardsVC.swift
@@ -9,32 +9,51 @@ import UIKit
 
 class PreviousCardsVC: UIViewController {
     var deck: [Deck] = []
+    var cardValues: [String] = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    var prevCards = GameVC().prevCards
+    //var prevCards : [String] = []
+    var passedPrevCards : [String] = []
+    
+    //let destinationVC = GameVC()
+        
+    
+    @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        //destinationVC.prevCards = passedPrevCards
+        
+        print(prevCards)
+        print(passedPrevCards)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
 }
 
 extension PreviousCardsVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return deck.count
+        return cardValues.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardCell") else {
-            return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        if (!prevCards.isEmpty) {
+            cell.textLabel?.text = "\(cardValues[indexPath.row]) = \(prevCards[indexPath.row])"
         }
-        
-        //cell.backgroundColor = colors[indexPath.row]
-        
-        cell.detailTextLabel?.text = "\(deck[indexPath.row])"
-        cell.textLabel?.text = "Reason #\(indexPath.row + 1)"
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you tapped me")
+    }
+    
+    
     
 }
 
